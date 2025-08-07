80f6bc53cf2a8aa5a534a04a6761439723884844
Christina Nemirich <nemirichkg@my.msu.ru>
2025-08-07 13:08:58 +0300

    Add the selector quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-﻿let productsHTML = "";
+﻿let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
@@ -25,7 +25,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -53,11 +53,11 @@ products.forEach((product) => {
     </div>`;
 });
 
-document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
     let matchingItem;
 
     cart.forEach((item) => {
@@ -66,12 +66,16 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    const itemQuantity = Number(quantitySelector.value);
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += itemQuantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: itemQuantity,
       });
     }
 
@@ -81,6 +85,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       cartQuantity += item.quantity;
     });
 
-    document.querySelector(".js-cart-quantity").innerHTML = `${cartQuantity}`;
+    document.querySelector('.js-cart-quantity').innerHTML = `${cartQuantity}`;
   });
 });
