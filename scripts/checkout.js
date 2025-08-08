b9a5ee5821c216ba7ec8437fb6d6c95e281648f2
Christina Nemirich <nemirichkg@my.msu.ru>
2025-08-09 00:25:21 +0300

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 ﻿import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
