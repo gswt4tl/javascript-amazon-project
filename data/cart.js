7d3ad0f64f1ebde31a98f23edcdf018d06ace864
Christina Nemirich <nemirichkg@my.msu.ru>
2025-08-09 17:17:48 +0300

    Create automated tests for orderSummary

--- data/cart.js
+++ data/cart.js
@@ -3,18 +3,22 @@
 loadFromStorage();
 
 export function loadFromStorage() {
-	cart = JSON.parse(localStorage.getItem('cart')) || [
-		{
-			productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-			quantity: 2,
-			deliveryOptionId: '1',
-		},
-		{
-			productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-			quantity: 1,
-			deliveryOptionId: '2',
-		},
-	];
+	cart = JSON.parse(localStorage.getItem('cart'));
+
+	if (!cart) {
+		cart = [
+			{
+				productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+				quantity: 2,
+				deliveryOptionId: '1',
+			},
+			{
+				productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+				quantity: 1,
+				deliveryOptionId: '2',
+			},
+		];
+	}
 }
 
 export function saveToStorage() {
@@ -30,30 +34,28 @@ export function addToCart(productId) {
 		}
 	});
 
-	// ХУЙ ЖОПА ЖОПА ЖОПА ЧЛЕН
-
-	// const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-	// const itemQuantity = Number(quantitySelector.value);
-	// if (matchingItem) {
-	// 	matchingItem.quantity += itemQuantity;
-	// } else {
-	// 	cart.push({
-	// 		productId,
-	// 		quantity: itemQuantity,
-	// 		deliveryOptionId: '1',
-	// 	});
-	// }
+	const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+	const itemQuantity = Number(quantitySelector.value);
 
 	if (matchingItem) {
-		matchingItem.quantity += 1;
+		matchingItem.quantity += itemQuantity;
 	} else {
 		cart.push({
 			productId,
-			quantity: 1,
+			quantity: itemQuantity,
 			deliveryOptionId: '1',
 		});
 	}
+
+	// if (matchingItem) {
+	// 	matchingItem.quantity += 1;
+	// } else {
+	// 	cart.push({
+	// 		productId,
+	// 		quantity: 1,
+	// 		deliveryOptionId: '1',
+	// 	});
+	// }
 
 	saveToStorage();
 }
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -17,7 +17,7 @@ export function renderOrderSummary() {
 		const dateString = calculateDeliveryDate(deliveryOption);
 
 		cartSummaryHTML += `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: ${dateString}</div>
   
             <div class="cart-item-details-grid">
@@ -30,7 +30,7 @@ export function renderOrderSummary() {
                     ${matchingProduct.name}
                 </div>
                 <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${
 			cartItem.quantity
 		}</span>
@@ -39,7 +39,7 @@ export function renderOrderSummary() {
                   <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                   <span class="save-quantity-link link-primary js-save-link"
                   data-product-id="${matchingProduct.id}">Save</span>
-                  <span class="delete-quantity-link link-primary js-delete-link"
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}"
                   data-product-id="${matchingProduct.id}">Delete</span>
                 </div>
                 </div>
