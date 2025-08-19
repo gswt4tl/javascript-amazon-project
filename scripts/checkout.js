462ec4a80fb315924ab2be56008f6e29d151bb96
Christina Nemirich <nemirichkg@my.msu.ru>
2025-08-19 16:48:40 +0300

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,10 @@
 ﻿import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+	renderCheckoutHeader();
+	renderOrderSummary();
+	renderPaymentSummary();
+});
