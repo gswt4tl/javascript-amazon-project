dd0d630d395ba16104656d4874fb8b17cdc1796b
Christina Nemirich <nemirichkg@my.msu.ru>
2025-08-19 16:37:30 +0300

    Add backend practice file

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,3 +1,8 @@
 ﻿const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://supersimplebackend.dev');
+
+xhr.addEventListener('load', () => {
+	console.log(xhr.response);
+});
+
+xhr.open('GET', 'https://supersimplebackend.dev/');
 xhr.send();
